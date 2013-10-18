04082914ab1724fa2392465fbb08fef927134d2e
Ismael Celis <ismaelct@gmail.com>
2013-10-18 16:51:45 +0100

    Comments

--- main.go
+++ main.go
@@ -16,15 +16,19 @@ func main() {
     backend_hosts string
     memcache_host    string
     api_host      string
+    udp_host  string
   )
   
   flag.StringVar(&listen_host, "listen", "localhost:3000", "HTTP host:port to listen for incoming requests")
   flag.StringVar(&backend_hosts, "backends", "", "Comma-separated list of host:port backend servers to proxy traffic to")
   flag.StringVar(&memcache_host, "memcache", "localhost:11211", "comma-separated host:port memcache host to cache requests in")
   flag.StringVar(&api_host, "api", "localhost:7000", "HTTP host:port to serve API from")
+  flag.StringVar(&udp_host, "udphost", "localhost:5555", "UDP host:port to send packets to")
 
   flag.Parse()
 
+  /* Caching proxy
+  ------------------------------------*/
   proxy, err := proxy.NewProxy(backend_hosts, memcache_host)
 
   if err != nil {
@@ -32,6 +36,8 @@ func main() {
   }
 
 
+  /* Control / stats API
+  -------------------------------------*/
   api, err := api.NewApi(proxy)
 
   if err != nil {
